7add09565c85777f6f8a93dd7d117ad14b43784b
Sky-Breaker07 <macguzik98@gmail.com>
2023-07-22 14:51:53 +0100

    openai

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 const http = require("http");
 const util = require("util");
 const EventEmitter = require('events');
+const express = require('express');
 
 // setInterval(() => {
 //     console.log(`Hello there. My name is ${utils.generateRandom(data.firstNames, 1)} ${utils.generateRandom(data.lastNames, 1)}. I live in ${(utils.generateRandom(data.countries, 1)).country}. I am a student of ${utils.generateRandom(data.universities, 1)}. My favorite animals include${utils.generateRandom(data.animalNames, 3)} and ${utils.generateRandom(data.animalNames, 1)}. My favorite colors include${utils.generateRandom(data.colors, 3)} and ${utils.generateRandom(data.colors, 1)}`)
@@ -217,3 +218,57 @@ const EventEmitter = require('events');
 // })
 
 // server.listen(5000)
+
+const { Configuration, OpenAIApi } = require("openai");
+const readlineSync = require("readline-sync");
+require("dotenv").config();
+
+(async () => {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const history = [];
+
+  while (true) {
+    const user_input = readlineSync.question("Your input: ");
+
+    const messages = [];
+    for (const [input_text, completion_text] of history) {
+      messages.push({ role: "user", content: input_text });
+      messages.push({ role: "assistant", content: completion_text });
+    }
+
+    messages.push({ role: "user", content: user_input });
+
+    try {
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+      });
+
+      const completion_text = completion.data.choices[0].message.content;
+      console.log(completion_text);
+
+      history.push([user_input, completion_text]);
+
+      const user_input_again = readlineSync.question(
+        "\nWould you like to continue the conversation? (Y/N)"
+      );
+      if (user_input_again.toUpperCase() === "N") {
+        return;
+      } else if (user_input_again.toUpperCase() !== "Y") {
+        console.log("Invalid input. Please enter 'Y' or 'N'.");
+        return;
+      }
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.status);
+        console.log(error.response.data);
+      } else {
+        console.log(error.message);
+      }
+    }
+  }
+})();
